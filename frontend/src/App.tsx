a876e6a06d119c1c8bea992845cd4c9f14de9ba7
Julia Demitraszek <juliademi05@gmail.com>
2025-07-07 21:15:59 +0200

    main components created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,21 @@ import './App.css'
 import Home from './pages/Home'
 import About from './pages/About'
 import Navbar from './pages/components/Navbar'
+import NotFound from './pages/NotFound'
+import Background from './pages/components/Background'
+import Footer from './pages/components/Footer'
 
 function App() {
   return (
     <>
       <Navbar/>
+      <Background/>
+      <Footer/>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/about" element={<About/>}/>
+          <Route path="*" element={<NotFound/>}/>
         </Routes>
       </BrowserRouter>
     </>
